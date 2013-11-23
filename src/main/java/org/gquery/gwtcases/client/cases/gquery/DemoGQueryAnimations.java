0584db065cb2ff6f0dc8fe02469b4c63535cb1db
Manuel Carrasco Moñino <manuel.carrasco.m@gmail.com>
2013-11-23 07:24:26 +0100

    gquery animations

--- src/main/java/org/gquery/gwtcases/client/cases/gquery/DemoGQueryAnimations.java
+++ src/main/java/org/gquery/gwtcases/client/cases/gquery/DemoGQueryAnimations.java
@@ -2,23 +2,16 @@ package org.gquery.gwtcases.client.cases.gquery;
 
 import static com.google.gwt.query.client.GQuery.*;
 
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.query.client.Function;
+import com.google.gwt.query.client.GQuery;
+import com.google.gwt.query.client.plugins.effects.Fx;
 import com.google.gwt.query.client.plugins.effects.ClipAnimation.Action;
 import com.google.gwt.query.client.plugins.effects.ClipAnimation.Corner;
 import com.google.gwt.query.client.plugins.effects.ClipAnimation.Direction;
-import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
-import com.google.gwt.safehtml.shared.SimpleHtmlSanitizer;
-import com.google.gwt.user.client.Random;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextArea;
-import com.google.gwt.user.client.ui.TextBox;
 
 public class DemoGQueryAnimations implements EntryPoint {
   
@@ -36,62 +29,34 @@ public class DemoGQueryAnimations implements EntryPoint {
     put("11 toggle", new Function(){public void f() {$(this).as(Effects).toggle(800);}});
     put("12 clipUp", new Function(){public void f() {$(this).as(Effects).clipUp();}});
     put("13 clipDown", new Function(){public void f() {$(this).as(Effects).clipDown();}});
-    put("14 clipDisappear", new Function(){public void f() {$(this).as(Effects).clipDisappear(8000);}});
+    put("14 clipDisappear", new Function(){public void f() {$(this).as(Effects).clipDisappear();}});
     put("15 clipAppear", new Function(){public void f() {$(this).as(Effects).clipAppear();}});
     put("16 clipToggle", new Function(){public void f() {$(this).as(Effects).clipToggle(800);}});
     put("17 clip custom", new Function(){public void f() {$(this).as(Effects).clip(Action.TOGGLE, Corner.TOP_RIGHT, Direction.BIDIRECTIONAL);}});
     put("18 animate red", new Function(){public void f() {$(this).as(Effects).animate($$("background-color: red, color: gold"));}});
     put("18 animate gold", new Function(){public void f() {$(this).as(Effects).animate($$("background-color: gold, color: red"));}});
-    put("19 rotation", new Function(){public void f() {$(this).as(Effects).animate($$("rotationY:360, transformOrigin:'left top'"));}});
-    
-    
+    put("19 rotation", new Function(){public void f() {$(this).as(Effects).animate($$("rotateY:180deg, rotateX:180deg, background:red, color: gold, transformOrigin:'center'"));}});
+    put("20 rotation", new Function(){public void f() {$(this).as(Effects).animate($$("rotateY:0deg, rotateX:0deg, background: gold, color: red, transformOrigin:'center'"));}});
   }};
 
   public void onModuleLoad() {
     
-    final TextArea ar = new TextArea();
-    final HTML h = new HTML();
-    Button b = new Button("B1");
-    Button b2 = new Button("B2");
+    final GQuery div = $("<div class='sample'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>")
+      .css($$("position: absolute, top: 20px, right: 20px"))
+      .css($$("border: solid 1px black, width: 100px, background: gold, font-size: 14px, padding: 15px"));
     
-
+//    Fx.css3 = false;
     
-    
-    RootPanel.get().add(ar);
-    RootPanel.get().add(h);
-    RootPanel.get().add(b);
-    RootPanel.get().add(b2);
-    
-    b.addClickHandler(new ClickHandler() {
-      public void onClick(ClickEvent event) {
-        SafeHtmlBuilder sb = new SafeHtmlBuilder();
-        sb.appendHtmlConstant("aaa" + Random.nextInt() + ">>>" + ar.getValue());
-        h.setHTML(sb.toSafeHtml());
-      }
-    });
-    
-    b2.addClickHandler(new ClickHandler() {
-      public void onClick(ClickEvent event) {
-        h.setHTML(SimpleHtmlSanitizer.sanitizeHtml(ar.getValue()));
-      }
-    });
-    
-    
-//    
-//    final GQuery div = $("<div class='sample'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>")
-//      .css($$("position: absolute, top: 20px, right: 20px"))
-//      .css($$("border: solid 1px black, width: 100px, background: gold, font-size: 14px, padding: 15px"));
-//    
-//    GQuery ul= $("<ul>");
-//    for (final Entry<String, Function> e : animations.entrySet()) {
-//      $("<li>" + e.getKey()).appendTo(ul).click(new Function(){public void f() {
-//        e.getValue().setElement(div
-//            //.stop(false, true)
-//            .get(0)).f();
-//      }});
-//    }
-//    ul.appendTo(document);
-//    div.appendTo(document);
+    GQuery ul= $("<ul>");
+    for (final Entry<String, Function> e : animations.entrySet()) {
+      $("<li>" + e.getKey()).appendTo(ul).click(new Function(){public void f() {
+        e.getValue().setElement(div
+            .stop(false, true)
+            .get(0)).f();
+      }});
+    }
+    ul.appendTo(document);
+    div.appendTo(document);
   }
   
 }
